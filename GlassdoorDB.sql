e1b7a0d91a3e9e75e8bcc32684536d778b61ed42
RajPotla <33848587+RajPotla@users.noreply.github.com>
2020-06-17 15:11:59 -0500

    Add files via upload

---
+++ GlassdoorDB.sql
@@ -0,0 +1,47 @@
+CREATE TABLE Company(
+	Company_ID INT PRIMARY KEY IDENTITY(1,1),
+	Company_Name VARCHAR(60),
+	HQ_City VARCHAR(30),
+	Sector VARCHAR(20),
+	Total_Employees INT,
+	Annual_Revenue INT,
+)
+
+Select * from Company
+
+CREATE TABLE Job(
+	Job_ID INT PRIMARY KEY IDENTITY(1,1),
+	Title VARCHAR(30),
+	Job_Description VARCHAR(400),
+	Date_Posted DATETIME,
+	Job_Status VARCHAR(10),
+	Company_ID INT FOREIGN KEY REFERENCES Company(Company_ID)
+)
+
+Select * FROM Job
+
+
+CREATE TABLE User_P(
+	User_ID INT PRIMARY KEY IDENTITY(1,1),
+	First_Name VARCHAR(30),
+	Last_Name VARCHAR(30),
+	E_mail VARCHAR(30),
+	Phone_Number INT,
+	date_created DATETIME
+);
+
+Select * FROM User_P
+
+CREATE TABLE Review(
+	ID INT PRIMARY KEY IDENTITY(1,1),
+	Title VARCHAR(20),
+	Description VARCHAR(400),
+	Date_Created DATETIME,
+	User_ID INT FOREIGN KEY REFERENCES User_P(User_ID),
+	Job_ID INT FOREIGN KEY REFERENCES Job(Job_ID)
+)
+
+Select * FROM Review
+Select * FROM User_P
+Select * FROM Job
+Select * from Company
